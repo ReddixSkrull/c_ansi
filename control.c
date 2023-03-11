f73b005a5757b0f82c1bdd392504f17ddcc983d8
ReddixSkrull <jannicolas-doering@web.de>
2023-03-11 17:04:29 +0100

    initial commit mit allen ansi farben und einer leeren main um die control codes zu testen, die main wird in der cmakelists mit kompiliert, sollte man das ganze als library verwendne wollen muss man aus dd_exectuabel ein add_library machen und die mian raus machen da librarys(eigentlich archive) keinen entrypoint benötigen

---
+++ control.c
@@ -0,0 +1,37 @@
+#include "control.h"
+
+char ansi_reset[] = "\x1b[0m";
+
+char ansi_text_foreground_black[] = "\x1b[30m";
+char ansi_text_foreground_red[] = "\x1b[31m";
+char ansi_text_foreground_green[] = "\x1b[32m";
+char ansi_text_foreground_yellow[] = "\x1b[33m";
+char ansi_text_foreground_blue[] = "\x1b[34m";
+char ansi_text_foreground_magenta[] = "\x1b[35m";
+char ansi_text_foreground_cyan[] = "\x1b[36m";
+char ansi_text_foreground_white[] = "\x1b[37m";
+char ansi_text_foreground_grey[] = "\x1b[90m";
+char ansi_text_foreground_brred[] = "\x1b[91m";
+char ansi_text_foreground_brgreen[] = "\x1b[92m";
+char ansi_text_foreground_bryellow[] = "\x1b[93m";
+char ansi_text_foreground_brblue[] = "\x1b[94m";
+char ansi_text_foreground_brmagenta[] = "\x1b[95m";
+char ansi_text_foreground_brcyan[] = "\x1b[96m";
+char ansi_text_foreground_brwhite[] = "\x1b[97m";
+
+char ansi_text_background_black[] = "\x1b[40m";
+char ansi_text_background_red[] = "\x1b[41m";
+char ansi_text_background_green[] = "\x1b[42m";
+char ansi_text_background_yellow[] = "\x1b[43m";
+char ansi_text_background_blue[] = "\x1b[44m";
+char ansi_text_background_magenta[] = "\x1b[45m";
+char ansi_text_background_cyan[] = "\x1b[46m";
+char ansi_text_background_white[] = "\x1b[47m";
+char ansi_text_background_grey[] = "\x1b[100m";
+char ansi_text_background_brred[] = "\x1b[101m";
+char ansi_text_background_brgreen[] = "\x1b[102m";
+char ansi_text_background_bryellow[] = "\x1b[103m";
+char ansi_text_background_brblue[] = "\x1b[104m";
+char ansi_text_background_brmagenta[] = "\x1b[105m";
+char ansi_text_background_brcyan[] = "\x1b[106m";
+char ansi_text_background_brwhite[] = "\x1b[107m";
---
+++ control.h
@@ -0,0 +1,37 @@
+#pragma once
+
+extern char ansi_reset[];
+
+extern char ansi_text_foreground_black[];
+extern char ansi_text_foreground_red[];
+extern char ansi_text_foreground_green[];
+extern char ansi_text_foreground_yellow[];
+extern char ansi_text_foreground_blue[];
+extern char ansi_text_foreground_magenta[];
+extern char ansi_text_foreground_cyan[];
+extern char ansi_text_foreground_white[];
+extern char ansi_text_foreground_grey[];
+extern char ansi_text_foreground_brred[];
+extern char ansi_text_foreground_brgreen[];
+extern char ansi_text_foreground_bryellow[];
+extern char ansi_text_foreground_brblue[];
+extern char ansi_text_foreground_brmagenta[];
+extern char ansi_text_foreground_brcyan[];
+extern char ansi_text_foreground_brwhite[];
+extern char ansi_text_background_black[];
+
+extern char ansi_text_background_red[];
+extern char ansi_text_background_green[];
+extern char ansi_text_background_yellow[];
+extern char ansi_text_background_blue[];
+extern char ansi_text_background_magenta[];
+extern char ansi_text_background_cyan[];
+extern char ansi_text_background_white[];
+extern char ansi_text_background_grey[];
+extern char ansi_text_background_brred[];
+extern char ansi_text_background_brgreen[];
+extern char ansi_text_background_bryellow[];
+extern char ansi_text_background_brblue[];
+extern char ansi_text_background_brmagenta[];
+extern char ansi_text_background_brcyan[];
+extern char ansi_text_background_brwhite[];
